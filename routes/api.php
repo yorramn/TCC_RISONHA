df78d2bad3e413fcfbeee91323d18503022bd518
yorramn <bielyorramn@gmail.com>
2021-09-10 14:59:16 -0300

    Inclusão do restante dos models e controller, migrations e criação da parte de vendas

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\PromocaoController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VendaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,13 +23,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 });
 */
+Route::group(['prefix' => '/user'], function () {
+    Route::get('/all', [UserController::class, 'index']);
+    Route::post('/store',[UserController::class,'store']);
+    Route::post('/login',[UserController::class,'login']);
+});
 Route::group(['middleware'=>['auth:sanctum']],function(){
-
-    Route::group(['prefix' => '/user'], function () {
-        Route::get('/all', [UserController::class, 'index']);
-        Route::post('/store',[UserController::class,'store']);
-        Route::post('/login',[UserController::class,'login']);
-    });
 
     Route::group(['prefix' => '/categoria'], function () {
         Route::get('/', [CategoriaController::class, 'index']);
@@ -36,7 +37,7 @@ Route::group(['middleware'=>['auth:sanctum']],function(){
         Route::put('/{id}', [CategoriaController::class, 'update']);
         Route::delete('/{id}', [CategoriaController::class, 'destroy']);
     });
-/*
+
     Route::group(['prefix' => '/produto'], function () {
         Route::get('/', [ProdutoController::class, 'index']);
         Route::get('/{id}', [ProdutoController::class, 'show']);
@@ -44,12 +45,27 @@ Route::group(['middleware'=>['auth:sanctum']],function(){
         Route::put('/{id}', [ProdutoController::class, 'update']);
         Route::delete('/{id}', [ProdutoController::class, 'destroy']);
     });
-*/
+
     Route::group(['prefix' => '/cliente'], function () {
         Route::get('/', [ClienteController::class, 'index']);
         Route::get('/{id}', [ClienteController::class, 'show']);
         Route::post('/', [ClienteController::class, 'store']);
         Route::put('/{id}', [ClienteController::class, 'update']);
         Route::delete('/{id}', [ClienteController::class, 'destroy']);
+    });
+
+    Route::group(['prefix' => '/promocao'], function () {
+        Route::get('/', [PromocaoController::class, 'index']);
+        Route::get('/{id}', [PromocaoController::class, 'show']);
+        Route::post('/', [PromocaoController::class, 'store']);
+        Route::put('/{id}', [PromocaoController::class, 'update']);
+        Route::delete('/{id}', [PromocaoController::class, 'destroy']);
+    });
+
+    Route::group(['prefix' => '/venda'], function () {
+        Route::get('/', [VendaController::class, 'index']);
+        Route::get('/{id}', [VendaController::class, 'show']);
+        Route::post('/', [VendaController::class, 'store']);
+        Route::put('/{id}', [VendaController::class, 'update']);
     });
 });
